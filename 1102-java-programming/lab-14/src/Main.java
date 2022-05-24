a25ad8d6cabc9dfb900de72f83ca83b2703ca2d0
楊斯惟 <yangszwei@outlook.com>
2022-05-24 12:02:39 +0800

    Upload 1102-java-programming/lab-14~16

---
+++ 1102-java-programming/lab-14/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        StringBuilder sb = new StringBuilder(scanner.nextLine());
+        sb.reverse();
+
+        for (char c : sb.toString().toCharArray())
+            System.out.print(c >= 'a' ? Character.toUpperCase(c) : Character.toLowerCase(c));
+    }
+}
